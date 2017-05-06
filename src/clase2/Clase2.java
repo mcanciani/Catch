5318d9ed4879abed2695edc137fb9baaf0defb8b
educacionit <educacionit@alumno-401-07>
2017-05-06 11:06:53 -0300

    cree el proyecto

---
+++ src/clase2/Clase2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author educacionit
+ */
+public class Clase2 {
+    
+
+/*public class estudiante extends Persona {
+   
+    public Estudiante (String nombre, String apellido, int edad)
+    {
+    super(nombre,apellido,edad);
+    }
+    }
+    psvm    
+    
+    
+    
+    */    
+}
---
+++ src/clase2/Zoologico.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author educacionit
+ */
+public class Zoologico {
+final int cantidad_animales = 25; 
+final int raciones_por_animal = 5;
+boolean open;
+
+
+public void abrir()
+{ 
+this.open = true;
+        }
+}
+
