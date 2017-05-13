5a6a4ed550d392372073d78e4c39cfd403a23030
educacionit <educacionit@alumno-401-07>
2017-05-13 14:20:07 -0300

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/claseerrores/PilaVaciaException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claseerrores;
+
+/**
+ *
+ * @author educacionit
+ */
+public class PilaVaciaException extends Exception{
+
+    public PilaVaciaException(String message) {
+        super(message);
+    }
+    
+    
+    
+}
